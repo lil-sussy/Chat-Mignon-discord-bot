f12a4e15e564ac7b37f4796812af30dd93db50f9
lil-sussy <naykeysnet@gmail.com>
2025-01-25 11:22:59 +0100

    solution : login manually to fetlife on an external proxy, save session-save-cookie and pass it to code. then. proceed to /events/near with same cookie and same IP

--- src/tests/fetlifeFuthertest copy.js
+++ src/tests/fetlifeFuthertest copy.js
@@ -9,20 +9,36 @@ const { CookieJar } = require("tough-cookie");
 const cookieJar = new CookieJar();
 const axiosInstance = axios.create({
 	baseURL: "https://fetlife.com",
-	withCredentials: true,
+	withCredentials: false, // Set to false to avoid sending cookies automatically
 	headers: {
+		"accept": "*/*",
+		"accept-encoding": "gzip, deflate, br, zstd",
+		"accept-language": "en-US,en;q=0.9,fr;q=0.8",
+		"cache-control": "no-cache",
+		"cookie": "language=en; fetlife_pwa=none; remember_user_token=eyJfcmFpbHMiOnsibWVzc2FnZSI6Ilcxc3hPRGs1TWpFM01GMHNJaVF5WVNReE1pUllhQzh4TUVaWVpYWmlSMjlrUjJkR1pHVkVNM0oxSWl3aU1UY3pOemM1T0RnM01pNDJNekF3TlRZNUlsMD0iLCJleHAiOiIyMDI1LTAyLTA4VDA5OjU0OjMyLjYzMFoiLCJwdXIiOiJjb29raWUucmVtZW1iZXJfdXNlcl90b2tlbiJ9fQ%3D%3D--edff78c1208bce8960c95d0fb13549f6bc910628; _fl_sessionid=19f3f79d766602742b5af1527f6fb118; __cf_bm=2u2a4uCO1ouI8kosqQRo3BLLDuAdx3wrwFllYFNZauU-1737798872-1.0.1.1-a7oc8PmUPQ0Yoq5RukJ1r4_JH3YNrpKjh9s9cW7H4p6nFIVi2B_BeaB0sXGIqisdKk7dngEaXm2X17ISrIK89pe_r1avOkXoyqMyQjGL6.s; _cfuvid=OE9cOkPlUK_G1kYBe0mzbXOI4zYlOjKEY7zN4xL98Vs-1737798872702-0.0.1.1-604800000; cf_clearance=EHjMdIURJGof.xD0SAJROfmq4npw4yMiBClRjrgbXH0-1737798873-1.2.1.1-7CUbFUPmPhRF5R.mRv44qQhUiMPOM1aZ2bEkQ7w3Azm2CBsMOePvpLlo.wQHjO9ClVfrruhP1vfHFudRLuDtd4nB6yICwKEyfpLqkrV9qN6xaJiKXP7V6Ywzlr54FVL3zXBkckYnLslm.3GJv2JAD5UMxRt1ZrjUIhcO1UozxqDSsuLgMvtBooxk03jlYCDvJVkUb_n06pZBd1frbsc7hoKSPur9raiq6IDOCTNWmjLu21P3rUjBm7C2J8cJCjL5hIkmrsBLCvQx2OvK_P8.ZLRsqkuFJMYnJUP04Cx8SfQ",
+		"pragma": "no-cache",
+		"priority": "u=1, i",
+		"sec-ch-ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+		"sec-ch-ua-arch": "\"x86\"",
+		"sec-ch-ua-bitness": "\"64\"",
+		"sec-ch-ua-full-version": "\"130.0.6723.193\"",
+		"sec-ch-ua-full-version-list": "\"Chromium\";v=\"130.0.6723.193\", \"Google Chrome\";v=\"130.0.6723.193\", \"Not?A_Brand\";v=\"99.0.0.0\"",
+		"sec-ch-ua-mobile": "?0",
+		"sec-ch-ua-model": "\"\"",
+		"sec-ch-ua-platform": "\"Linux\"",
+		"sec-ch-ua-platform-version": "\"6.12.10\"",
+		"sec-fetch-dest": "empty",
+		"sec-fetch-mode": "no-cors",
+		"sec-fetch-site": "same-origin",
 		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.132 Safari/537.36",
-		"Accept-Language": "en-US,en;q=0.9",
-		"Accept-Encoding": "gzip, deflate, br, zstd",
-		"Content-Type": "application/x-www-form-urlencoded",
-		"Upgrade-Insecure-Requests": "1",
-		"Sec-Fetch-Dest": "document",
-		"Sec-Fetch-Mode": "navigate",
-		"Sec-Fetch-Site": "same-origin",
-		"Sec-Fetch-User": "?1",
-		Priority: "u=0, i",
-		Pragma: "no-cache",
-		"Cache-Control": "no-cache",
+		"Referer": "https://fetlife.com/login",
+		"Origin": "https://fetlife.com",
+	},
+	timeout: 5000, // Set a timeout for requests
+	maxRedirects: 5, // Limit the number of redirects
+	responseType: 'document', // Set response type to document
+	validateStatus: function (status) {
+		return status >= 200 && status < 300; // Resolve only if the status code is less than 300
 	},
 });
 axiosCookieJarSupport(axiosInstance);
@@ -32,17 +48,18 @@ async function fetchCsrfToken() {
 	const signInPage = await axiosInstance.get(`/login`, {
 		headers: {
 			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
-				Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-				"Accept-Language": "en-US,en;q=0.5",
-				"Accept-Encoding": "gzip, deflate, br, zstd",
-				Referer: "https://fetlife.com/logged_out",
-				"Upgrade-Insecure-Requests": "1",
-				"Sec-Fetch-Dest": "document",
-				"Sec-Fetch-Mode": "navigate",
-				"Sec-Fetch-Site": "same-origin",
-				"Sec-Fetch-User": "?1",
-				Connection: "keep-alive",
-				Cookie: "language=en; fetlife_pwa=none; ",
+			Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+			"Accept-Language": "en-US,en;q=0.5",
+			"Accept-Encoding": "gzip, deflate, br, zstd",
+			Referer: "https://fetlife.com/logged_out",
+			"Upgrade-Insecure-Requests": "1",
+			"Sec-Fetch-Dest": "document",
+			"Sec-Fetch-Mode": "navigate",
+			"Sec-Fetch-Site": "same-origin",
+			"Sec-Fetch-User": "?1",
+			Connection: "keep-alive",
+			Cookie:
+					"language=en; _cfuvid=bGHXyKTYlobwXS_7t6yMj6Y8qgNdotK9Nc6mLLi2PSA-1736477463445-0.0.1.1-604800000; fetlife_pwa=none; _fl_sessionid=653d1fd8dcdb09797e3de869763d5a7b; __cf_bm=NFK4ONvbhGa.sSZ.Quz_OEONDBz3uq6kxVlH.lEIlLE-1736477463-1.0.1.1-SqhiEvO3opeOXClz56lvLts6WedZBIC4FmMq2YprsTr62htULj1R9nPi9Mru0oXwfoKLXe8DbokpPDZsrUUo0Z9kVLwUesWoGZB4EDRA7ss; cf_clearance=RrdV0ivIzVrhanYjA1KvjY2Hfeu_miH2CGp9qCyN15Q-1736477464-1.2.1.1-RD7FoF1xDCGxosBpHcrplmoCG3F65zcgraUh7GtgU6hyqzASW1aDOm4x_0F3uG9LUoFDAgX.7281oiMgpIm_12E1YqxoH8QtmIxydbr31VON_.GxuWZOvRE_hIMGNGoEnwH3LDMw6K_OuQQkWCm2Uhb6OI3YGzoGkUx_KmIKYkvLTg427FX.pH.A7E.rtxQpwe_wKVoPOEHqY27WnAJ43g4BNi_wVt4zmIYPEGIOYnPsCxDV0IaHt_eZDyqOMFShlHVJrwqrnPB0Yk7eqnab1hfEVBJL3Tel9sQi2U0rm2DqsiXG4BiLEteNr_pFVwu7mlB73Drymmoo2Td5h2Rfhw; remember_user_token=eyJfcmFpbHMiOnsibWVzc2FnZSI6Ilcxc3lNRGd3TkRjd05WMHNJaVF5WVNReE1pUTFUM052T0VsSlZVUnVhVkI0ZWs4dk5tNXhMbTVsSWl3aU1UY3pOalEzTnpRNE15NDNOakEzTWpNMElsMD0iLCJleHAiOiIyMDI1LTAxLTI0VDAyOjUxOjIzLjc2MFoiLCJwdXIiOiJjb29raWUucmVtZW1iZXJfdXNlcl90b2tlbiJ9fQ%3D%3D--07fb71169dd175450bf671260464ef82e3a698db",
 		},
 	});
 
@@ -102,66 +119,65 @@ async function fetchCookiesFromUrls(urls) {
 // Function to fetch the login page
 async function fetchLoginPage() {
 	try {
-		const urls = [
-			"https://gav2.fetlife.com/vite/assets/i18n-en-B32nWQ9x.js",
-			"https://gav2.fetlife.com/vite/assets/application_style-BJSyJdCu.js",
-			"https://gav2.fetlife.com/vite/assets/i18n-DokRD9wR.js",
-			"https://gav2.fetlife.com/vite/assets/application-eEsEIcSA.js",
-			"https://gav2.fetlife.com/vite/assets/controller-CrcZEXPm.js",
-			"https://gav2.fetlife.com/vite/assets/tags_input-Bz3J-wsg.js",
-			"https://gav2.fetlife.com/vite/assets/form_validator_controller-C5VsjArb.js",
-			"https://gav2.fetlife.com/vite/assets/index-DKqD94ZC.js",
-			"https://gav2.fetlife.com/vite/assets/dom_ready-DM2Vf_4Q.js",
-			"https://gav2.fetlife.com/vite/assets/edit_tags-3aC6V8EV.js",
-			"https://gav2.fetlife.com/vite/assets/index-BriLBh9p.js",
-			"https://gav2.fetlife.com/vite/assets/push_notifications_firebase-D0-dJ6DL.js",
-			"https://gav2.fetlife.com/vite/assets/polyfills-legacy-CBq8z5GW.js",
-			"https://gav2.fetlife.com/vite/assets/application-legacy-DwJHEEKc.js",
-			"https://gav2.fetlife.com/vite/assets/i18n-en-legacy-DGf0JAVa.js",
-		];
-		const cookiesMap = await fetchCookiesFromUrls(urls);
-
-		// Now use the pre-configured axiosInstance
-		const data = await fetchEventsNear();
-
-    const events = [];
-		
-		for (const event of data) {
-			const eventID = event.id; // Assuming each event object has an 'id' property
-      events.append(await fetchEventRsvps(eventID))
-		}
-
-    console.log(events)
+		// const urls = [
+		// 	"https://gav2.fetlife.com/vite/assets/i18n-en-B32nWQ9x.js",
+		// 	"https://gav2.fetlife.com/vite/assets/application_style-BJSyJdCu.js",
+		// 	"https://gav2.fetlife.com/vite/assets/i18n-DokRD9wR.js",
+		// 	"https://gav2.fetlife.com/vite/assets/application-eEsEIcSA.js",
+		// 	"https://gav2.fetlife.com/vite/assets/controller-CrcZEXPm.js",
+		// 	"https://gav2.fetlife.com/vite/assets/tags_input-Bz3J-wsg.js",
+		// 	"https://gav2.fetlife.com/vite/assets/form_validator_controller-C5VsjArb.js",
+		// 	"https://gav2.fetlife.com/vite/assets/index-DKqD94ZC.js",
+		// 	"https://gav2.fetlife.com/vite/assets/dom_ready-DM2Vf_4Q.js",
+		// 	"https://gav2.fetlife.com/vite/assets/edit_tags-3aC6V8EV.js",
+		// 	"https://gav2.fetlife.com/vite/assets/index-BriLBh9p.js",
+		// 	"https://gav2.fetlife.com/vite/assets/push_notifications_firebase-D0-dJ6DL.js",
+		// 	"https://gav2.fetlife.com/vite/assets/polyfills-legacy-CBq8z5GW.js",
+		// 	"https://gav2.fetlife.com/vite/assets/application-legacy-DwJHEEKc.js",
+		// 	"https://gav2.fetlife.com/vite/assets/i18n-en-legacy-DGf0JAVa.js",
+		// ];
+		// const cookiesMap = await fetchCookiesFromUrls(urls);
 
 		let response = await axiosInstance.get("/login", {
-			Referer: "https://fetlife.com/",
-			Origin: "https://fetlife.com",
+			Referer: "https://fetlife.com/login",
+			Origin: "https://fetlife.com/login",
+      withCredentials: false
 		});
 
 		const setCookies = response.headers["set-cookie"] || [];
 		setCookies.forEach((cookie) => {
 			const cookieName = cookie.split("=")[0];
 			cookiesMap.set(cookieName, cookie);
-			cookieJar.setCookieSync(cookie, `${fetlifeBaseUrl}/login`);
+			cookieJar.setCookieSync(cookie, `${fetlifeBaseUrl}`);
 		});
 
 		let html = response.data;
-		const csrfToken = findCsrfToken(html);
+		// const csrfToken = findCsrfToken(html);
+		const data = await fetchEventsNear();
+
+		const events = [];
+
+		for (const event of data) {
+			const eventID = event.id; // Assuming each event object has an 'id' property
+			events.append(await fetchEventRsvps(eventID));
+		}
+
+		console.log(events);
 		fs.writeFileSync("./src/tests/cringe/login.html", html, "utf8");
 
-		const postData = new URLSearchParams({
-			"user[login]": process.env.FETLIFE_USERNAME,
-			"user[password]": process.env.FETLIFE_PASSWORD,
-			"user[locale]": "en",
-			"user[otp_attempt]": "step_2",
-			authenticity_token: csrfToken,
-		});
+		// const postData = new URLSearchParams({
+		// 	"user[login]": process.env.FETLIFE_USERNAME,
+		// 	"user[password]": process.env.FETLIFE_PASSWORD,
+		// 	"user[locale]": "en",
+		// 	"user[otp_attempt]": "step_2",
+		// 	authenticity_token: csrfToken,
+		// });
 
-		response = await axiosInstance.post("/login", postData.toString(), {
-			headers: {
-				Referrer: "https://fetlife.com/login",
-			}
-		});
+		// response = await axiosInstance.post("/login", postData.toString(), {
+		// 	headers: {
+		// 		Referrer: "https://fetlife.com/login",
+		// 	}
+		// });
 
 		html = response.data;
 		fs.writeFileSync("./src/tests/cringe/postLogin.html", html, "utf8");
@@ -182,41 +198,42 @@ async function fetchLoginPage() {
 async function fetchEventsNear() {
 	try {
 		const now = new Date();
-		const fromDate = now.toISOString().split('T')[0]; // YYYY-MM-DD
+		const fromDate = now.toISOString().split("T")[0]; // YYYY-MM-DD
 		const fromTime = now.toISOString(); // Full ISO string
 
-		const response = await axiosInstance.get('/events/near', {
+		const response = await axiosInstance.get("/events/near", {
 			params: {
 				page: 1,
-				'categories[]': 'bdsm_party',
-				mapbox_location_id: 'locality.510541',
-				'search[long]': 2.383964,
-				'search[lat]': 48.889484,
-				'search[range]': 50000,
-				'search[place_name]': '19th arrondissement, Paris, Île-de-France, France',
-				'search[bbox]': '2.364728,48.872049,2.41082,48.902148',
+				"categories[]": "bdsm_party",
+				mapbox_location_id: "locality.510541",
+				"search[long]": 2.383964,
+				"search[lat]": 48.889484,
+				"search[range]": 50000,
+				"search[place_name]": "19th arrondissement, Paris, Île-de-France, France",
+				"search[bbox]": "2.364728,48.872049,2.41082,48.902148",
 				from_date: fromDate,
-				from_time: fromTime
+				from_time: fromTime,
 			},
 			headers: {
-				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0',
-				Accept: 'application/json',
-				'Accept-Language': 'en-US,en;q=0.5',
-				'Accept-Encoding': 'gzip, deflate, br, zstd',
-				Referer: 'https://fetlife.com/events/near',
-				'Content-Type': 'application/json',
-				'Sec-Fetch-Dest': 'empty',
-				'Sec-Fetch-Mode': 'cors',
-				'Sec-Fetch-Site': 'same-origin',
-				Connection: 'keep-alive',
-				Cookie: 'language=en; _cfuvid=bGHXyKTYlobwXS_7t6yMj6Y8qgNdotK9Nc6mLLi2PSA-1736477463445-0.0.1.1-604800000; fetlife_pwa=none; _fl_sessionid=653d1fd8dcdb09797e3de869763d5a7b; __cf_bm=NFK4ONvbhGa.sSZ.Quz_OEONDBz3uq6kxVlH.lEIlLE-1736477463-1.0.1.1-SqhiEvO3opeOXClz56lvLts6WedZBIC4FmMq2YprsTr62htULj1R9nPi9Mru0oXwfoKLXe8DbokpPDZsrUUo0Z9kVLwUesWoGZB4EDRA7ss; cf_clearance=RrdV0ivIzVrhanYjA1KvjY2Hfeu_miH2CGp9qCyN15Q-1736477464-1.2.1.1-RD7FoF1xDCGxosBpHcrplmoCG3F65zcgraUh7GtgU6hyqzASW1aDOm4x_0F3uG9LUoFDAgX.7281oiMgpIm_12E1YqxoH8QtmIxydbr31VON_.GxuWZOvRE_hIMGNGoEnwH3LDMw6K_OuQQkWCm2Uhb6OI3YGzoGkUx_KmIKYkvLTg427FX.pH.A7E.rtxQpwe_wKVoPOEHqY27WnAJ43g4BNi_wVt4zmIYPEGIOYnPsCxDV0IaHt_eZDyqOMFShlHVJrwqrnPB0Yk7eqnab1hfEVBJL3Tel9sQi2U0rm2DqsiXG4BiLEteNr_pFVwu7mlB73Drymmoo2Td5h2Rfhw; remember_user_token=eyJfcmFpbHMiOnsibWVzc2FnZSI6Ilcxc3lNRGd3TkRjd05WMHNJaVF5WVNReE1pUTFUM052T0VsSlZVUnVhVkI0ZWs4dk5tNXhMbTVsSWl3aU1UY3pOalEzTnpRNE15NDNOakEzTWpNMElsMD0iLCJleHAiOiIyMDI1LTAxLTI0VDAyOjUxOjIzLjc2MFoiLCJwdXIiOiJjb29raWUucmVtZW1iZXJfdXNlcl90b2tlbiJ9fQ%3D%3D--07fb71169dd175450bf671260464ef82e3a698db'
-			}
+				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
+				Accept: "application/json",
+				"Accept-Language": "en-US,en;q=0.5",
+				"Accept-Encoding": "gzip, deflate, br, zstd",
+				Referer: "https://fetlife.com/events/near",
+				"Content-Type": "application/json",
+				"Sec-Fetch-Dest": "empty",
+				"Sec-Fetch-Mode": "cors",
+				"Sec-Fetch-Site": "same-origin",
+				Connection: "keep-alive",
+				Cookie:
+					"language=en; _cfuvid=bGHXyKTYlobwXS_7t6yMj6Y8qgNdotK9Nc6mLLi2PSA-1736477463445-0.0.1.1-604800000; fetlife_pwa=none; _fl_sessionid=653d1fd8dcdb09797e3de869763d5a7b; __cf_bm=NFK4ONvbhGa.sSZ.Quz_OEONDBz3uq6kxVlH.lEIlLE-1736477463-1.0.1.1-SqhiEvO3opeOXClz56lvLts6WedZBIC4FmMq2YprsTr62htULj1R9nPi9Mru0oXwfoKLXe8DbokpPDZsrUUo0Z9kVLwUesWoGZB4EDRA7ss; cf_clearance=RrdV0ivIzVrhanYjA1KvjY2Hfeu_miH2CGp9qCyN15Q-1736477464-1.2.1.1-RD7FoF1xDCGxosBpHcrplmoCG3F65zcgraUh7GtgU6hyqzASW1aDOm4x_0F3uG9LUoFDAgX.7281oiMgpIm_12E1YqxoH8QtmIxydbr31VON_.GxuWZOvRE_hIMGNGoEnwH3LDMw6K_OuQQkWCm2Uhb6OI3YGzoGkUx_KmIKYkvLTg427FX.pH.A7E.rtxQpwe_wKVoPOEHqY27WnAJ43g4BNi_wVt4zmIYPEGIOYnPsCxDV0IaHt_eZDyqOMFShlHVJrwqrnPB0Yk7eqnab1hfEVBJL3Tel9sQi2U0rm2DqsiXG4BiLEteNr_pFVwu7mlB73Drymmoo2Td5h2Rfhw; remember_user_token=eyJfcmFpbHMiOnsibWVzc2FnZSI6Ilcxc3lNRGd3TkRjd05WMHNJaVF5WVNReE1pUTFUM052T0VsSlZVUnVhVkI0ZWs4dk5tNXhMbTVsSWl3aU1UY3pOalEzTnpRNE15NDNOakEzTWpNMElsMD0iLCJleHAiOiIyMDI1LTAxLTI0VDAyOjUxOjIzLjc2MFoiLCJwdXIiOiJjb29raWUucmVtZW1iZXJfdXNlcl90b2tlbiJ9fQ%3D%3D--07fb71169dd175450bf671260464ef82e3a698db",
+			},
 		});
 
-		console.log('Response Data:', response.data);
+		console.log("Response Data:", response.data);
 		return response.data;
 	} catch (error) {
-		console.error('Error fetching events near:', error.message);
+		console.error("Error fetching events near:", error.message);
 	}
 }
 
@@ -224,19 +241,20 @@ async function fetchEventRsvps(eventID) {
 	try {
 		const response = await axiosInstance.get(`/events/${eventID}/rsvps`, {
 			headers: {
-				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0',
-				Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-				'Accept-Language': 'en-US,en;q=0.5',
-				'Accept-Encoding': 'gzip, deflate, br, zstd',
+				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
+				Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+				"Accept-Language": "en-US,en;q=0.5",
+				"Accept-Encoding": "gzip, deflate, br, zstd",
 				Referer: `https://fetlife.com/events/${eventID}`,
-				'Upgrade-Insecure-Requests': '1',
-				'Sec-Fetch-Dest': 'document',
-				'Sec-Fetch-Mode': 'navigate',
-				'Sec-Fetch-Site': 'same-origin',
-				'Sec-Fetch-User': '?1',
-				Connection: 'keep-alive',
-				Cookie: 'language=en; _cfuvid=bGHXyKTYlobwXS_7t6yMj6Y8qgNdotK9Nc6mLLi2PSA-1736477463445-0.0.1.1-604800000; fetlife_pwa=none; _fl_sessionid=653d1fd8dcdb09797e3de869763d5a7b; __cf_bm=NFK4ONvbhGa.sSZ.Quz_OEONDBz3uq6kxVlH.lEIlLE-1736477463-1.0.1.1-SqhiEvO3opeOXClz56lvLts6WedZBIC4FmMq2YprsTr62htULj1R9nPi9Mru0oXwfoKLXe8DbokpPDZsrUUo0Z9kVLwUesWoGZB4EDRA7ss; cf_clearance=RrdV0ivIzVrhanYjA1KvjY2Hfeu_miH2CGp9qCyN15Q-1736477464-1.2.1.1-RD7FoF1xDCGxosBpHcrplmoCG3F65zcgraUh7GtgU6hyqzASW1aDOm4x_0F3uG9LUoFDAgX.7281oiMgpIm_12E1YqxoH8QtmIxydbr31VON_.GxuWZOvRE_hIMGNGoEnwH3LDMw6K_OuQQkWCm2Uhb6OI3YGzoGkUx_KmIKYkvLTg427FX.pH.A7E.rtxQpwe_wKVoPOEHqY27WnAJ43g4BNi_wVt4zmIYPEGIOYnPsCxDV0IaHt_eZDyqOMFShlHVJrwqrnPB0Yk7eqnab1hfEVBJL3Tel9sQi2U0rm2DqsiXG4BiLEteNr_pFVwu7mlB73Drymmoo2Td5h2Rfhw; remember_user_token=eyJfcmFpbHMiOnsibWVzc2FnZSI6Ilcxc3lNRGd3TkRjd05WMHNJaVF5WVNReE1pUTFUM052T0VsSlZVUnVhVkI0ZWs4dk5tNXhMbTVsSWl3aU1UY3pOalEzTnpRNE15NDNOakEzTWpNMElsMD0iLCJleHAiOiIyMDI1LTAxLTI0VDAyOjUxOjIzLjc2MFoiLCJwdXIiOiJjb29raWUucmVtZW1iZXJfdXNlcl90b2tlbiJ9fQ%3D%3D--07fb71169dd175450bf671260464ef82e3a698db'
-			}
+				"Upgrade-Insecure-Requests": "1",
+				"Sec-Fetch-Dest": "document",
+				"Sec-Fetch-Mode": "navigate",
+				"Sec-Fetch-Site": "same-origin",
+				"Sec-Fetch-User": "?1",
+				Connection: "keep-alive",
+				Cookie:
+					"language=en; _cfuvid=bGHXyKTYlobwXS_7t6yMj6Y8qgNdotK9Nc6mLLi2PSA-1736477463445-0.0.1.1-604800000; fetlife_pwa=none; _fl_sessionid=653d1fd8dcdb09797e3de869763d5a7b; __cf_bm=NFK4ONvbhGa.sSZ.Quz_OEONDBz3uq6kxVlH.lEIlLE-1736477463-1.0.1.1-SqhiEvO3opeOXClz56lvLts6WedZBIC4FmMq2YprsTr62htULj1R9nPi9Mru0oXwfoKLXe8DbokpPDZsrUUo0Z9kVLwUesWoGZB4EDRA7ss; cf_clearance=RrdV0ivIzVrhanYjA1KvjY2Hfeu_miH2CGp9qCyN15Q-1736477464-1.2.1.1-RD7FoF1xDCGxosBpHcrplmoCG3F65zcgraUh7GtgU6hyqzASW1aDOm4x_0F3uG9LUoFDAgX.7281oiMgpIm_12E1YqxoH8QtmIxydbr31VON_.GxuWZOvRE_hIMGNGoEnwH3LDMw6K_OuQQkWCm2Uhb6OI3YGzoGkUx_KmIKYkvLTg427FX.pH.A7E.rtxQpwe_wKVoPOEHqY27WnAJ43g4BNi_wVt4zmIYPEGIOYnPsCxDV0IaHt_eZDyqOMFShlHVJrwqrnPB0Yk7eqnab1hfEVBJL3Tel9sQi2U0rm2DqsiXG4BiLEteNr_pFVwu7mlB73Drymmoo2Td5h2Rfhw; remember_user_token=eyJfcmFpbHMiOnsibWVzc2FnZSI6Ilcxc3lNRGd3TkRjd05WMHNJaVF5WVNReE1pUTFUM052T0VsSlZVUnVhVkI0ZWs4dk5tNXhMbTVsSWl3aU1UY3pOalEzTnpRNE15NDNOakEzTWpNMElsMD0iLCJleHAiOiIyMDI1LTAxLTI0VDAyOjUxOjIzLjc2MFoiLCJwdXIiOiJjb29raWUucmVtZW1iZXJfdXNlcl90b2tlbiJ9fQ%3D%3D--07fb71169dd175450bf671260464ef82e3a698db",
+			},
 		});
 
 		const html = response.data;
--- src/tests/fetlifepuppeteer.js
+++ src/tests/fetlifepuppeteer.js
@@ -1,5 +1,7 @@
 const puppeteer = require("puppeteer-extra");
+const fs = require("fs"); // Import the fs module
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const randomUseragent = require('random-useragent');
 
 puppeteer.use(StealthPlugin());
 
@@ -17,48 +19,51 @@ async function loginWithPuppeteer() {
 	});
 	const page = await browser.newPage();
 
+	// Set a random user agent
+	const userAgent = randomUseragent.getRandom();
+	await page.setUserAgent(userAgent);
+
 	// Set the headers for the page
 	await page.setExtraHTTPHeaders({
-		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.132 Safari/537.36",
-		"Accept-Language": "en-US,en;q=0.9",
-		"Accept-Encoding": "gzip, deflate, br, zstd",
-		"Content-Type": "application/x-www-form-urlencoded",
-		"Upgrade-Insecure-Requests": "1",
-		"Sec-Fetch-Dest": "document",
-		"Sec-Fetch-Mode": "navigate",
-		"Sec-Fetch-Site": "same-origin",
-		"Sec-Fetch-User": "?1",
-		Priority: "u=0, i",
-		Pragma: "no-cache",
-		"Cache-Control": "no-cache",
+		"accept": "*/*",
+		"accept-encoding": "gzip, deflate, br, zstd",
+		"accept-language": "en-US,en;q=0.9,fr;q=0.8",
+		"cache-control": "no-cache",
+		"cookie": "language=en; fetlife_pwa=none; remember_user_token=eyJfcmFpbHMiOnsibWVzc2FnZSI6Ilcxc3hPRGs1TWpFM01GMHNJaVF5WVNReE1pUllhQzh4TUVaWVpYWmlSMjlrUjJkR1pHVkVNM0oxSWl3aU1UY3pOemM1T0RnM01pNDJNekF3TlRZNUlsMD0iLCJleHAiOiIyMDI1LTAyLTA4VDA5OjU0OjMyLjYzMFoiLCJwdXIiOiJjb29raWUucmVtZW1iZXJfdXNlcl90b2tlbiJ9fQ%3D%3D--edff78c1208bce8960c95d0fb13549f6bc910628; _fl_sessionid=19f3f79d766602742b5af1527f6fb118; __cf_bm=2u2a4uCO1ouI8kosqQRo3BLLDuAdx3wrwFllYFNZauU-1737798872-1.0.1.1-a7oc8PmUPQ0Yoq5RukJ1r4_JH3YNrpKjh9s9cW7H4p6nFIVi2B_BeaB0sXGIqisdKk7dngEaXm2X17ISrIK89pe_r1avOkXoyqMyQjGL6.s; _cfuvid=OE9cOkPlUK_G1kYBe0mzbXOI4zYlOjKEY7zN4xL98Vs-1737798872702-0.0.1.1-604800000; cf_clearance=EHjMdIURJGof.xD0SAJROfmq4npw4yMiBClRjrgbXH0-1737798873-1.2.1.1-7CUbFUPmPhRF5R.mRv44qQhUiMPOM1aZ2bEkQ7w3Azm2CBsMOePvpLlo.wQHjO9ClVfrruhP1vfHFudRLuDtd4nB6yICwKEyfpLqkrV9qN6xaJiKXP7V6Ywzlr54FVL3zXBkckYnLslm.3GJv2JAD5UMxRt1ZrjUIhcO1UozxqDSsuLgMvtBooxk03jlYCDvJVkUb_n06pZBd1frbsc7hoKSPur9raiq6IDOCTNWmjLu21P3rUjBm7C2J8cJCjL5hIkmrsBLCvQx2OvK_P8.ZLRsqkuFJMYnJUP04Cx8SfQ",
+		"pragma": "no-cache",
+		"priority": "u=1, i",
+		"sec-ch-ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+		"sec-ch-ua-arch": "\"x86\"",
+		"sec-ch-ua-bitness": "\"64\"",
+		"sec-ch-ua-full-version": "\"130.0.6723.193\"",
+		"sec-ch-ua-full-version-list": "\"Chromium\";v=\"130.0.6723.193\", \"Google Chrome\";v=\"130.0.6723.193\", \"Not?A_Brand\";v=\"99.0.0.0\"",
+		"sec-ch-ua-mobile": "?0",
+		"sec-ch-ua-model": "\"\"",
+		"sec-ch-ua-platform": "\"Linux\"",
+		"sec-ch-ua-platform-version": "\"6.12.10\"",
+		"sec-fetch-dest": "empty",
+		"sec-fetch-mode": "no-cors",
+		"sec-fetch-site": "same-origin",
+		"Referer": "https://fetlife.com/login",
+		"Origin": "https://fetlife.com",
 	});
 
 	// Go to the login page, wait for basic DOM to load
 	await page.goto("https://fetlife.com/login", { waitUntil: "domcontentloaded" });
 
-	// Update the selectors if necessary
-	const usernameSelector = 'input[name="user[login]"]'; // Verify this selector
-	const passwordSelector = 'input[name="user[password]"]'; // Verify this selector
-
 	try {
-		// Wait for the username input to be available
-		await page.waitForSelector(usernameSelector, { timeout: 5000 });
-		// Wait for the password input to be available
-		await page.waitForSelector(passwordSelector, { timeout: 5000 });
-
-		// Fill in the login form
-		await page.type(usernameSelector, process.env.FETLIFE_USERNAME);
-		await page.type(passwordSelector, process.env.FETLIFE_PASSWORD);
-
-		// Submit the form and wait for navigation
-		await Promise.all([page.click('button[type="submit"]'), page.waitForNavigation({ waitUntil: "networkidle2" })]);
-
-		// Log or inspect the page content once logged in
-		// Wait for Cloudflare check (when the title no longer contains "Just a moment...")
 		await page.waitForFunction(() => document.title && !document.title.includes("Just a moment..."), { timeout: 600000 });
 		await page.waitForFunction(() => document.title && !document.title.includes("Checking if the site connection is secure..."), { timeout: 60000 });
+		await page.waitForFunction(() => document.title && !document.title.includes("please unblock challenges.cloudflare.com to proceed."), { timeout: 60000 });
 		const content = await page.content();
-		console.log(content);
+    fs.writeFileSync("./src/tests/cringe/login.html", content, "utf8");
+
+		await page.goto("https://fetlife.com/events/near", { waitUntil: "domcontentloaded" });
+		await page.waitForFunction(() => document.title && !document.title.includes("please unblock challenges.cloudflare.com to proceed."), { timeout: 60000 });
+		await page.waitForFunction(() => document.title && !document.title.includes("Login | FetLife"), { timeout: 60000 });
+		await page.waitForFunction(() => document.title && !document.title.includes("Just a moment..."), { timeout: 600000 });
+		await page.waitForFunction(() => document.title && !document.title.includes("Checking if the site connection is secure..."), { timeout: 60000 });
+    fs.writeFileSync("./src/tests/cringe/events.html", content, "utf8");
 	} catch (error) {
 		console.error("Error during login process:", error.message);
 	} finally {
